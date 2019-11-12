3e90902f46f45f17b718338f59778831990cca79
marcosecchi <loziosecchi@gmail.com>
2019-11-12 10:45:50 +0100

    Add comments.

--- Assets/Scripts/VampireController.cs
+++ Assets/Scripts/VampireController.cs
@@ -5,6 +5,7 @@ public class VampireController : MonoBehaviour
 {
     private NavMeshAgent _agent;
 
+    // A gameObject in scene displaying the target point of the agent
     public GameObject targetPoint;
 
     void Start()
@@ -14,19 +15,21 @@ public class VampireController : MonoBehaviour
 
     void Update()
     {
+        // When the left mouse button is hit...
         if(Input.GetMouseButtonDown(0))
         {
+            // ...find the point that was clicked and...
             RaycastHit hit;
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             var hasHit = Physics.Raycast(ray, out hit, 100);
+
+            //  ... if something has been hit ...
             if(hasHit)
             {
+                // ... set the destination for this agent
                 _agent.destination = hit.point;
                 targetPoint.transform.position = hit.point;
             }
-
         }
-
-
     }
 }
--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -8,11 +8,12 @@ public class ZombieController : MonoBehaviour
 
     private GameObject _player;
 
+    // The distance used by the zombie to find a target
     public float smellSense = 5;
 
-    public Animator brain;
-
+    // A state that shows if the zombie is seeking the player
     private bool _isSeeking;
+
     private void Start()
     {
         // Find the navmesh agent component
@@ -24,14 +25,16 @@ public class ZombieController : MonoBehaviour
 
     private void Update()
     {
+        // Find the distance to the player
         var distanceToTarget = Vector3.Distance(
             transform.position, 
             _player.transform.position
             );
 
+        // If the player is within range ...
         if(distanceToTarget < smellSense)
         {
-            // moves toward the player, updating the destination each frame
+            // ... move toward the player, updating the destination each frame
             _agent.destination = _player.transform.position;
             _isSeeking = true;
         }
@@ -39,11 +42,14 @@ public class ZombieController : MonoBehaviour
         {
             _isSeeking = false;
         }
-
     }
 
+    /// <summary>
+    /// This function is called by the Unity engine and is used to draw gizmos in scene
+    /// </summary>
     private void OnDrawGizmosSelected()
     {
+        // Depending on the status, change the handles color
         if(_isSeeking)
         {
             Handles.color = new Color(1f, 0f, 0f, 0.1f);
@@ -52,6 +58,7 @@ public class ZombieController : MonoBehaviour
         {
             Handles.color = new Color(0f, 1f, 0f, 0.1f);
         }
+        // Draw a disc displaying the smell sense range for the zombie
         Handles.DrawSolidDisc(transform.position, Vector3.up, smellSense);
     }
 }
