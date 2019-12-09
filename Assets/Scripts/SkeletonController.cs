e26fb62e78368de3e61d5b230a69a587d07037ed
Marco Secchi <loziosecchi@gmail.com>
2019-12-09 17:19:49 +0100

    Add character controller scene.

---
+++ Assets/Scripts/SkeletonController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(CharacterController))]
+public class SkeletonController: MonoBehaviour
+{
+    CharacterController _controller;
+
+    public float speed = 4;
+    public float rotationSpeed = 4;
+
+    public CinemachineVirtualCamera secondaryCamera;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        var activateCamera = Input.GetButton("Fire1");
+        secondaryCamera.gameObject.SetActive(activateCamera);
+
+        var hMove = Input.GetAxis("Horizontal");
+        var rotation = rotationSpeed * hMove;
+        var rotVector = new Vector3(0, rotation, 0);
+        transform.Rotate(rotVector);
+
+        var forward =
+            transform.TransformDirection(Vector3.forward);
+        var vMove = Input.GetAxis("Vertical");
+        vMove = Mathf.Clamp(vMove, 0, 1);
+        var currentSpeed = rotationSpeed * vMove;
+        var move = currentSpeed * forward;
+        _controller.SimpleMove(move);
+    }
+}
